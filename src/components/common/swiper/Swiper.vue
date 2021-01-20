af2eb07ddceac547c3c5e10c079883d85b3c9443
xl <leixing999@126.com>
2021-01-20 21:18:18 +0800

    '增加轮播图'

---
+++ src/components/common/swiper/Swiper.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="swiper">
+    <div class="imgBox">
+      <!-- <a :href="link"><img :src="image" alt=""></a> -->
+      <ul>
+        <li v-for="(item,index) in swiperData" class="imgList" :key="index" :style="{display: index===currentIndex ? 'block' : 'none'}" >
+          <a :href="item.link">
+             <img :src="item.image" alt="" >
+          </a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="numBox">
+      <div class="nums" v-for="(item,index) in swiperData" :key="index" @click="btnClick(index)">
+        {{index+1}}
+      </div>
+     
+    </div>
+     
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Swiper',
+  props: {
+    swiperData: {
+      type: Array,
+      default() {
+        return []
+      },
+    },
+  },
+  data() {
+    return {
+      currentIndex: 0,
+      preIndex: 0,
+      nextIndex: 0,
+      isActive: false,
+    }
+  },
+  computed: {
+    link() {
+      /******异步导致数据没有加载的时候返回空值******* */
+      if (this.swiperData.length === 0) {
+        return ''
+      }
+      return this.swiperData[this.currentIndex].link
+    },
+    image() {
+      /******异步导致数据没有加载的时候返回空值******* */
+      if (this.swiperData.length === 0) {
+        return ''
+      }
+      return this.swiperData[this.currentIndex].image
+    },
+    activeStyle() {
+      return ''
+    },
+  },
+  methods: {
+    btnClick(index) {
+      this.currentIndex = index
+    },
+  },
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+li {
+  list-style: none;
+}
+.swiper {
+  height: 165px;
+  margin: 0 auto;
+}
+
+.imgBox {
+  height: 150px;
+  margin: 1px auto;
+}
+.imgList {
+  position: absolute;
+  display: none;
+}
+.imgList img {
+  width: 100%;
+  vertical-align: middle;
+}
+
+.numBox {
+  display: flex;
+  height: 20px;
+  margin-top: -10px;
+  margin-left: 65%;
+  position: fixed; /*让数字框浮动在图片层上*/
+  text-align: center;
+  color: white;
+}
+.nums {
+  flex: 1;
+  margin-right: 20px;
+  background: rgba(0, 255, 179, 0.4);
+}
+/* .numSpan {
+  color: white;
+  font-size: 30px;
+  margin-top: 20px;
+  line-height: 60px;
+} */
+/*
+.numSpan {
+  color: white;
+  font-size: 30px;
+  margin-top: 20px;
+  line-height: 60px;
+} */
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,6 +4,9 @@
       <nav-bar>
        <div slot="center" backgroudcolor="blue">购物街</div>
       </nav-bar>
+       <div>
+         <swiper :swiperData="banners"></swiper>
+       </div>
     </div>
     
   </div>
@@ -11,10 +14,23 @@
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
-
+import Swiper from 'components/common/swiper/Swiper'
+import { getHomeMultidata } from 'network/home'
 export default {
   name: 'Home',
-  components: { NavBar },
+  components: { NavBar, Swiper },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    }
+  },
+  created() {
+    getHomeMultidata().then((res) => {
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+    })
+  },
 }
 </script>
 
