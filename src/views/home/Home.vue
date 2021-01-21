8af1787985ab625c1f3628883052132d679388ac
xl <leixing999@126.com>
2021-01-21 08:49:27 +0800

    '增加推荐标签'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -7,6 +7,9 @@
        <div>
          <home-swiper :swiperData="banners"></home-swiper>
        </div>
+       <div>
+         <recommend-view :recommends="recommends"></recommend-view>
+       </div>
     </div>
     
   </div>
@@ -15,10 +18,11 @@
 <script>
 import NavBar from 'components/common/navbar/NavBar'
 import HomeSwiper from './homeComponents/HomeSwiper'
+import RecommendView from './homeComponents/RecommendView'
 import { getHomeMultidata } from 'network/home'
 export default {
   name: 'Home',
-  components: { NavBar, HomeSwiper },
+  components: { NavBar, HomeSwiper, RecommendView },
   data() {
     return {
       banners: [],
