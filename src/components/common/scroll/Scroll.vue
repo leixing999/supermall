bb649258497738fb0f738575a724f1a3483ac77d
xl <leixing999@126.com>
2021-01-22 22:22:08 +0800

    '增加better-scroll滚动控件'

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+export default {
+  name: 'Scroll',
+  data() {
+    return {
+      scroll: null,
+    }
+  },
+  mounted() {
+    this.scroll = new BScroll(this.$refs.wrapper, {
+      click: true,
+      tap: true,
+      wheel: true,
+    })
+  },
+}
+</script>
+
+<style scoped>
+</style>
